7e6f6e04e4165d1ff81133bdfa5339360c77b994
Ngoc Doan <ndoan@soka.edu>
2023-05-28 16:20:29 +0700

    display map, marker, and search bar

--- client/src/Map/Map.js
+++ client/src/Map/Map.js
@@ -1,13 +1,43 @@
-import React, { Fragment } from 'react'
+import React, { useState } from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import SearchBar from './SearchBar.js'
+const API_KEY = "AIzaSyDI4HgUw9Aw8eMrXx5jcAXvimyJLB4CS9g"
 
-const Map = () => {
+
+const containerStyle = {
+  width: '100%',
+  height: '100vh'
+};
+
+const center = {
+  lat: 33.56,
+  lng: -117.72
+};
+
+const onLoad = marker => {
+  console.log('marker: ', marker)
+}
+
+function MapContainer() {
+  const [selected, setSelected] = useState(null);
   return (
-    <Fragment>
-        <div>Map</div>
-        <h1>This is Map component</h1>
-    </Fragment>
-    
+    <LoadScript
+      googleMapsApiKey={API_KEY}
+      libraries={["places"]}
+    >
+      <div className="search-bar-container">
+        <SearchBar setSelected={setSelected} />
+      </div>
+
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        {selected && <Marker position={selected} onLoad={onLoad}  />}
+      </GoogleMap>
+    </LoadScript>
   )
 }
 
-export default Map;
+export default MapContainer;
---
+++ client/src/Map/SearchBar.js
@@ -0,0 +1,52 @@
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+    ComboboxOptionText,
+} from "@reach/combobox";
+import "./SearchBar.css"
+import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+} from "use-places-autocomplete";
+import React, { useState } from 'react'
+
+const SearchBar = ({ setSelected }) => {
+    const {
+        ready,
+        value,
+        setValue,
+        suggestions: { status, data },
+        clearSuggestions,
+    } = usePlacesAutocomplete();
+
+    const handleSelect = async(address) => {
+        setValue(address, false)
+        clearSuggestions();
+
+        const results = await getGeocode({address});
+        const {lat, lng} = await getLatLng(results[0]);
+        setSelected({lat, lng})
+    }
+    return (
+        <Combobox onSelect={handleSelect}>
+            <ComboboxInput 
+                value={value} 
+                onChange={(e) => setValue(e.target.value)} 
+                disabled={!ready} 
+                className="combobox-input" 
+                placeholder="Search an address" />
+            <ComboboxPopover>
+                <ComboboxList>
+                    { status === "OK" && data.map(({place_id, description}) => (<ComboboxOption key={place_id} value={description}/>))}
+                </ComboboxList>
+            </ComboboxPopover>
+        
+        </Combobox>
+    )
+}
+
+export default SearchBar;
+
