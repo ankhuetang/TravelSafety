836da11754e3da9b5941a47ad52f3956ec7d5f1c
Minh Phuong T Cao <88098962+ctmphuongg@users.noreply.github.com>
2023-06-03 21:54:33 -0400

    Map Controller (#7)
    * Map controller and model design modification
    
    * Map controller get place by address
    
    * update place model
    
    * update safety model and map controllers
    
    * Map controller - response return traffic
    
    * Map controller complete place
    
    ---------
    
    Co-authored-by: ankhuetang <ktang@friars.providence.edu>

---
+++ server/util/apis/traffic.js
@@ -0,0 +1,37 @@
+//call traffic APIs for data
+const axios = require('axios');
+const getBoundsFromLatLng = require('./bounding');
+require('dotenv').config({ path: '../../.env' });
+
+const API_KEY = process.env.TRAFFIC_API_KEY;
+// const location = { lat: 41.3873974, lng: 2.168568 };
+
+async function getTrafficInfo(location) {
+	let boundingBox;
+	try {
+		boundingBox = getBoundsFromLatLng(location.lat, location.lng, 1);
+	} catch (error) {
+		console.log('Cant calculate boundingBox');
+	}
+
+	const response = await axios.get(
+		`http://dev.virtualearth.net/REST/v1/Traffic/Incidents/${boundingBox}?key=${API_KEY}`
+	);
+
+	const data = response.data;
+
+	if (!data || data.statusCode !== 200) {
+		throw new Error('No data available');
+	}
+	return data;
+}
+
+// getTrafficInfo(location)
+// 	.then((result) => {
+// 		console.log(result.resourceSets[0].resources[0]);
+// 	})
+// 	.catch((error) => {
+// 		console.log(error);
+// 	});
+
+module.exports = getTrafficInfo;
