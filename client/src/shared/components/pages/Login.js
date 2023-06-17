8dfca6cde3afc9405f0211aba96d87f80f73d9f7
Cuong <vnguyen24@amherst.edu>
2023-06-18 00:06:29 +0700

    Change Login.js indent to 2

--- client/src/shared/components/pages/Login.js
+++ client/src/shared/components/pages/Login.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-	return (
-		<div>
-			<h1>This is the login page</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>This is the login page. Work in progress</h1>
+    </div>
+  );
 };
 
 export default Login;
