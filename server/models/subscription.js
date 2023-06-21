980e461186222d08aa6d54a4f3672a18cf96966b
MinhPhuong Cao <mtc013@bucknell.edu>
2023-06-20 20:12:52 -0400

    ??

--- server/models/subscription.js
+++ server/models/subscription.js
@@ -7,6 +7,7 @@ const subscriptionSchema = new Schema({
 	radius: { type: Number, required: true },
 	duration: { type: Number, required: true },
 	creator: { type: mongoose.Types.ObjectId, required: false, ref: 'User' },
+	expireAt: { type: Date, default: null, index: { expires: 0 } },
 });
 
 module.exports = mongoose.model('Subscription', subscriptionSchema);
