9d4111834ecdea0147279067ba17888941e422f1
Ngoc Doan <ndoan@soka.edu>
2023-07-01 17:03:34 +0700

    request data from server and show marker at the searched location

--- client/src/Map/Map.js
+++ client/src/Map/Map.js
@@ -1,49 +1,53 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
-import SearchBar from './SearchBar.js'
-import "./Map.css"
-import colors from "./data/ColorRange.js"
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import SearchBar from "./SearchBar.js";
+import "./Map.css";
+import colors from "./data/ColorRange.js";
 import {
   faBuildingCircleXmark,
   faBuildingCircleExclamation,
   faBuildingCircleCheck,
-  faTrafficCone
-} from '@fortawesome/free-solid-svg-icons'
-import InfoWindowContent from './InfoWindowContent.js'
+  faTrafficCone,
+} from "@fortawesome/free-solid-svg-icons";
+import InfoWindowContent from "./InfoWindowContent.js";
+import ColorBar from "./data/ColorBar.js";
 
-const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-const MAP_ID = process.env.REACT_APP_GOOGLE_MAPS_ID
+const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+const MAP_ID = process.env.REACT_APP_GOOGLE_MAPS_ID;
 
 // make it into user's location
 const center = {
-	lat: 33.56,
-	lng: -117.72,
+  lat: 33.56,
+  lng: -117.72,
 };
 
 function MapContainer() {
-  const [libraries] = useState(['places']);
+  const [libraries] = useState(["places"]);
   const [map, setMap] = useState(null);
   const [location, setLocation] = useState(null);
   const [markers, setMarkers] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
 
   const onLoad = useCallback((map) => {
-    setMap(map)
+    setMap(map);
   });
 
   useEffect(() => {
     if (location) {
-      // safety score
+      console.log(location)
       const level = Math.floor(Math.random() * 100) + 1;
       let icon;
       if (level < 40) {
-        icon = faBuildingCircleXmark.icon[4]
-      }
-      else if (level < 60) {
-        icon = faBuildingCircleExclamation.icon[4]
-      }
-      else {
-        icon = faBuildingCircleCheck.icon[4]
+        icon = faBuildingCircleXmark.icon[4];
+      } else if (level < 60) {
+        icon = faBuildingCircleExclamation.icon[4];
+      } else {
+        icon = faBuildingCircleCheck.icon[4];
       }
       const newMarker = {
         position: location,
@@ -56,39 +60,36 @@ function MapContainer() {
           scale: 0.05,
         },
         content: {
-          "overall": 44,
-          "lgbtq": 37,
-          "medical": 69,
-          "physicalHarm": 34,
-          "politicalFreedom": 50,
-          "theft": 42,
-          "women": 33
-        }
-      }
-      console.log("The safety level of this place is ", level)
-      setMarkers((prevMarkers) => [...prevMarkers, newMarker])
+          overall: 44,
+          lgbtq: 37,
+          medical: 69,
+          physicalHarm: 34,
+          politicalFreedom: 50,
+          theft: 42,
+          women: 33,
+        },
+      };
+      console.log("The safety level of this place is ", level);
+      setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
     }
-  }, [location])
+  }, [location]);
 
   const handleDrag = () => {
-    const center = map.getCenter()
-    const bounds = map.getBounds()
+    const center = map.getCenter();
+    const bounds = map.getBounds();
     console.log("Center:", center);
     console.log("Bounds:", bounds);
-  }
-  
+  };
+
   return (
-    <LoadScript
-      googleMapsApiKey={API_KEY}
-      libraries={libraries}
-    >
+    <LoadScript googleMapsApiKey={API_KEY} libraries={libraries}>
+      <ColorBar />
       <div className="map-container">
         <div className="search-bar-container">
           <SearchBar setLocation={setLocation} />
           {console.log(location)}
         </div>
         <div className="map">
-          {console.log("how many times you are repeatibf")}
           <GoogleMap
             mapContainerClassName="map-inner"
             center={center}
@@ -97,13 +98,14 @@ function MapContainer() {
             onLoad={onLoad}
             onDragEnd={handleDrag}
           >
-            {markers && markers.map((marker) => (
-              <Marker
-                position={marker.position}
-                icon={marker.icon}
-                onClick={() => setSelectedMarker(marker)}
-              />
-            ))}
+            {markers &&
+              markers.map((marker) => (
+                <Marker
+                  position={marker.position}
+                  icon={marker.icon}
+                  onClick={() => setSelectedMarker(marker)}
+                />
+              ))}
             {selectedMarker && (
               <InfoWindow position={selectedMarker.position}>
                 <InfoWindowContent content={selectedMarker.content} />
@@ -113,7 +115,7 @@ function MapContainer() {
         </div>
       </div>
     </LoadScript>
-  )
+  );
 }
 
 export default MapContainer;
--- client/src/Map/SearchBar.js
+++ client/src/Map/SearchBar.js
@@ -1,32 +1,49 @@
-import "./SearchBar.css"
+import "./SearchBar.css";
 import { StandaloneSearchBox } from "@react-google-maps/api";
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
+import axios from 'axios';
 
 const SearchBar = ({ setLocation }) => {
-    const searchInput = useRef();
-    const handleLocation = () => {
-        const place = searchInput.current.getPlaces();
-        if (place) {
-            console.log("place: ", place[0])
-            const lat = place[0].geometry.location.lat()
-            const lng = place[0].geometry.location.lng()
-            setLocation({lat, lng})      
-        }
+  const searchInput = useRef();
+  const fetchData = async (request) => {
+    try {
+      const response = await axios.post('/api/map/data', request);
+      setLocation(response.data);
+    } catch (error) {
+      console.log(error.message);
     }
-    return (
-        <StandaloneSearchBox 
-            onLoad={ref => searchInput.current = ref}
-            onPlacesChanged={handleLocation}>
-                <input 
-                    type="text"
-                    placeholder="Search an adress"
-                    className="combobox-input"
-                />
-        </StandaloneSearchBox>
-    )
-}
+  }
+  const handleLocation = () => {
+    const place = searchInput.current.getPlaces();
+    if (place) {
+      console.log("place: ", place[0]);
+      const address = place[0].formatted_address
+      const lat = place[0].geometry.location.lat();
+      const lng = place[0].geometry.location.lng();
+      console.log(lat, lng)
+      const request = {
+        address: address,
+        coordinates: {
+          lat: lat,
+          lng: lng
+        }, 
+        radius: 5
+      }
+      fetchData(request);
+    }
+  };
+  return (
+    <StandaloneSearchBox
+      onLoad={(ref) => (searchInput.current = ref)}
+      onPlacesChanged={handleLocation}
+    >
+      <input
+        type="text"
+        placeholder="Search an adress"
+        className="combobox-input"
+      />
+    </StandaloneSearchBox>
+  );
+};
 
 export default SearchBar;
-
-
-
