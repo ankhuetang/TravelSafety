ca059cea2d842f5efaaac4f5f403ca18ecf9b0d1
Cuong <vnguyen24@amherst.edu>
2023-07-01 21:09:59 +0700

    create profile page

---
+++ client/src/context/types.js
@@ -0,0 +1,17 @@
+export const GET_ALERTS = "GET_ALERTS";
+export const ADD_ALERT = "ADD_ALERT";
+export const FILTER_ALERTS = "FILTER_ALERTS";
+export const CLEAR_SUBSCRIBES = "CLEAR_SUBSCRIBES";
+export const CLEAR_FILTER = "CLEAR_FILTER";
+export const SET_WARNING = "SET_WARNING";
+export const REMOVE_WARNING = "REMOVE_WARNING";
+export const ALERT_ERROR = "ALERT_ERROR";
+
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
