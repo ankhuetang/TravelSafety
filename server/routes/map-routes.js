35531f74ee6c7398de4086babbfc0b584046af8e
ankhuetang <ktang@friars.providence.edu>
2023-07-01 00:23:16 -0400

    routes + auth for users

--- server/routes/map-routes.js
+++ server/routes/map-routes.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const { check } = require('express-validator');
+
 const mapController = require('../controllers/map-controllers');
 const subscriptionController = require('../controllers/subscription-controllers');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
-router.post('/data', mapController.getSearchByAddress);
+router.post(
+	'/data',
+	[check('address').not().isEmpty()],
+	mapController.getSearchByAddress
+);
 
-router.post('/subscription', subscriptionController.subscribe);
+//check for authentication for protected routes
+router.use(checkAuth);
+
+router.post(
+	'/subscription',
+	[
+		check('address').not().isEmpty(),
+		check('radius').not().isEmpty(),
+		check('duration').not().isEmpty(),
+	],
+	subscriptionController.subscribe
+);
 
 router.get('/profile', subscriptionController.getSubscription);
 
