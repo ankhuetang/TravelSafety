4e5deca6e900231f701a180032e53a10998e2e03
ankhuetang <ktang@friars.providence.edu>
2023-07-10 19:08:55 -0400

    endpoint for userData

--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -25,4 +25,14 @@ router.post(
 	usersController.login
 );
 
+router.get('/data', checkAuth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+});
+
 module.exports = router;
