5365d5c0dd353fde22b60c46bd92e061ba259b66
ankhuetang <ktang@friars.providence.edu>
2023-07-10 21:34:25 -0400

    debug frontend errors

--- server/app.js
+++ server/app.js
@@ -1,23 +1,25 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const mapRoutes = require('./routes/map-routes');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authoriztion'
-	);
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+// app.use((req, res, next) => {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader(
+// 		'Access-Control-Allow-Headers',
+// 		'Origin, X-Requested-With, Content-Type, Accept, Authoriztion'
+// 	);
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-	next();
-});
+// 	next();
+// });
 
 app.use('/api/map', mapRoutes);
 
