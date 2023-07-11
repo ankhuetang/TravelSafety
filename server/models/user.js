4a4895e280ac0a60c747b950cf01f36c8e4f15be
ankhuetang <ktang@friars.providence.edu>
2023-07-10 22:27:05 -0400

    edit user model to debug signup

--- server/models/user.js
+++ server/models/user.js
@@ -5,14 +5,10 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
 	name: { type: String, required: true },
-	phone: { type: String, required: true, unique: true },
+	phone: { type: String, required: false, unique: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true, minlength: 6 },
-	location: {
-		lat: { type: Number, required: true },
-		lng: { type: Number, required: true },
-	},
-	places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }],
+	places: [{ type: mongoose.Types.ObjectId, required: false, ref: 'Place' }],
 });
 
 userSchema.plugin(uniqueValidator);
