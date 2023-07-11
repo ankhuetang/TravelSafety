95e97e35a9d1630cdaa4bf4ac847b9f90e0db7e6
Ngoc Doan <ndoan@soka.edu>
2023-07-11 23:25:12 +0700

    style navbar

--- client/src/shared/components/layout/NavBar.js
+++ client/src/shared/components/layout/NavBar.js
@@ -1,37 +1,31 @@
-// TODO: Color styling for the buttons
-// CHANGE THIS!!!
-
 import React from "react";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <div>
-      <Navbar className="navbar-translucent">
+      <Navbar className="navbar-translucent" expand="lg">
         <Container>
-          <Navbar.Brand className="travel-safety">
-            {" "}
-            <i class="fa-solid fa-suitcase"></i> TravelSafety
+          <Navbar.Brand href="/" className="travel-safety-logo">
+            <span className="logo-circle">
+              <i className="fas fa-suitcase"></i>
+            </span>
+            <span className="logo-text">SafeTraveler</span>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/" className="home-link">
-                Home
+            <Nav className="ml-auto">
+              <Nav.Link href="/profile" className="nav-link">
+                Profile
               </Nav.Link>
-            </Nav>
-            <Nav className="justify-content-end">
-              <Nav.Link href="/profile">
-                <Button variant="light">Profile</Button>
+              <Nav.Link href="/register" className="nav-link">
+                Register
               </Nav.Link>
-              <Nav.Link href="/register">
-                <Button variant="light">Register</Button>
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <Button variant="light">Login</Button>
+              <Nav.Link href="/login" className="nav-link">
+                Login
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
@@ -42,3 +36,4 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
