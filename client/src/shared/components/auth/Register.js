e56939a4bdbe22f3cee5fe3410cd1dde434b73b1
Ngoc Doan <ndoan@soka.edu>
2023-07-14 15:13:57 +0700

    rebrand the web app

--- client/src/shared/components/auth/Register.js
+++ client/src/shared/components/auth/Register.js
@@ -3,6 +3,19 @@ import React, { useContext, useState, useEffect } from "react";
 import WarningContext from "../../../context/warning/WarningContext";
 import AuthContext from "../../../context/auth/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { CssVarsProvider } from "@mui/joy/styles";
+import {
+  GlobalStyles,
+  CssBaseline,
+  Box,
+  Button,
+  FormControl,
+  Input,
+  Typography,
+  FormLabel,
+} from "@mui/joy";
+import customTheme from "../../theme";
+import { root, formSide, formContainer, form, imageSide } from "../styles";
 
 const Register = () => {
   const warningContext = useContext(WarningContext);
@@ -37,6 +50,14 @@ const Register = () => {
 
   const { name, email, phone, password, password2 } = user;
 
+  const labels = [
+    "Name",
+    "Email",
+    "Phone Number",
+    "Password",
+    "Confirm Password",
+  ];
+
   const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
   const onSubmit = (e) => {
@@ -57,70 +78,59 @@ const Register = () => {
   };
 
   return (
-    <div className="form-container">
-      <h1>
-        Account <span className="text-primary">Register</span>
-      </h1>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email Address</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="phone">Phone Number</label>
-          <input
-            type="text"
-            name="phone"
-            value={phone}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={onChange}
-            required
-            minLength="6"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password2">Confirm Password</label>
-          <input
-            type="password"
-            name="password2"
-            value={password2}
-            onChange={onChange}
-            required
-            minLength="6"
-          />
-        </div>
-        <input
-          type="submit"
-          value="Register"
-          className="btn btn-primary btn-block"
-        />
-      </form>
-    </div>
+    <CssVarsProvider
+      defaultMode="light"
+      disableTransitionOnChange
+      theme={customTheme}
+    >
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          ":root": root,
+        }}
+      />
+      <Box sx={(theme) => formSide}>
+        <Box sx={formContainer}>
+          <Box component="main" sx={form}>
+            <div>
+              <Typography component="h1" fontSize="xl2" fontWeight="lg">
+                Create a new account
+              </Typography>
+              <Typography level="body2" sx={{ my: 1, mb: 3 }}>
+                Welcome to Safe Traveler - your go-to app for exploring and
+                staying informed about the safety of places
+              </Typography>
+            </div>
+            <form onSubmit={onSubmit}>
+              {Object.entries(user).map(([key, value], index) => (
+                <FormControl required>
+                  <FormLabel>{labels[index]}</FormLabel>
+                  <Input
+                    name={key}
+                    value={value}
+                    onChange={onChange}
+                    type={
+                      key === "password" || key === "password2"
+                        ? "password"
+                        : "text"
+                    }
+                  />
+                </FormControl>
+              ))}
+              <Button type="submit" fullWidth>
+                Sign in
+              </Button>
+            </form>
+          </Box>
+          <Box component="footer" sx={{ py: 3 }}>
+            <Typography level="body3" textAlign="center">
+              © Safe Traveler {new Date().getFullYear()}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box sx={(theme) => imageSide} />
+    </CssVarsProvider>
   );
 };
 
